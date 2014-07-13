9fcc7e224e8a3fe07e5ff69ea8e0dca4db19905d
aksjd <akshay.jd@hotmail.com>
2014-07-13 15:05:31 +0530

    WebSocket Test

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ io.on('connection', function (socket) {
 io.on('connection', function (socket) {
     console.log('a user connected');
     socket.on('chat message', function (msg) {
-        io.emit('chat message', msg);
+        io.emit('chat message', msg+"Test");
     });
 
 });
